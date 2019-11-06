aa24aad6d3fbc6d996740b89186229806f0538f3
antoine.junot <antoine.junot@iut-rodez.fr>
2019-11-06 12:15:30 +0100

    fix #2 Récupération et affichage des utilisateurs avec énoncé non préparé

---
+++ all_users.php
@@ -0,0 +1,57 @@
+<!doctype html>
+<html lang="fr">
+	<head>
+		<meta charset="utf-8">
+		<title>premier pas en PDO</title>
+		<!-- Bootstrap CSS -->
+		<link href="bootstrap/css/bootstrap.css" rel="stylesheet">
+		<link href="font-awesome/css/font-awesome.css" rel="stylesheet">
+		<!-- CSS -->
+		<link href="css/monStyle.css" rel="stylesheet">
+		
+		<?php
+			/* link a la BD */
+			$host = 'localhost';
+			$db   = 'my_activities';
+			$user = 'root';
+			$pass = 'root';
+			$charset = 'utf8mb4';
+			$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+			$options = [
+				PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+				PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+				PDO::ATTR_EMULATE_PREPARES   => false,
+			];
+			try {
+				 $pdo = new PDO($dsn, $user, $pass, $options);
+			} catch (PDOException $e) {
+				 throw new PDOException($e->getMessage(), (int)$e->getCode());
+			}
+		?>
+	</head>
+	
+	<body>
+		<table>
+			<tr>
+				<th>ID</th>
+				<th>Username</th>
+				<th>Email</th>
+				<th>Status</th>
+			</tr>
+			<tr>
+				<?php
+					$stmt = $pdo->query('SELECT * FROM users');
+					while ($row = $stmt->fetch()) {
+				?>
+						<td><?php echo $row['id'];?></td><br>
+						<td><?php echo $row['username'] ;?><td><br>
+						<td><?php echo $row['email'];?></td><br>
+						<td><?php echo $row['status_id'];?></td><br>
+				<?php
+					}
+				?>
+			</tr>
+		</table>
+
+	</body>
+</html>
